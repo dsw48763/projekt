3a9c37d814d808b53d7ec3cb22b72365ace9f4ec
Patryk <neprooo@wp.pl>
2023-05-23 00:45:43 +0200

    Task2

--- projekt.py
+++ projekt.py
@@ -1,6 +1,14 @@
 import sys
-
+import json
 
 #task1
 arguments = sys.argv[1:]
 print("Passed arguments:", arguments)
+#task2
+json_file = "file.json"
+try:
+    with open(json_file, "r") as file:
+        data = json.load(file)
+    print("Loaded JSON data:", data)
+except Exception as e:
+    print("Failed to load JSON data from file:", e)
