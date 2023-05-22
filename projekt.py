1f7d299713222cbbaa9ecab8584de5d74b5486af
Patryk <neprooo@wp.pl>
2023-05-23 00:47:00 +0200

    Task4

--- projekt.py
+++ projekt.py
@@ -1,6 +1,6 @@
 import sys
 import json
-
+import yaml
 #task1
 arguments = sys.argv[1:]
 print("Passed arguments:", arguments)
@@ -21,3 +21,12 @@ try:
     print("JSON data saved to", output_file)
 except Exception as e:
     print("Failed to save JSON data:", e)
+
+#task4
+yml_file = "file.yml"
+try:
+    with open(yml_file, "r") as file:
+        data = yaml.safe_load(file)
+    print("Loaded YAML data:", data)
+except Exception as e:
+    print("Failed to load YAML data from file:", e)
