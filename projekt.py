a7fdc33573f81bfa46015b66b4341eec54555c35
Patryk <neprooo@wp.pl>
2023-05-23 00:47:31 +0200

    Task5

--- projekt.py
+++ projekt.py
@@ -30,3 +30,11 @@ try:
     print("Loaded YAML data:", data)
 except Exception as e:
     print("Failed to load YAML data from file:", e)
+#task5
+output_file = "output.yml"
+try:
+    with open(output_file, "w") as file:
+        yaml.dump(data, file)
+    print("YAML data saved to", output_file)
+except Exception as e:
+    print("Failed to save YAML data:", e)
